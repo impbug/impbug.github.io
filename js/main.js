16afbd96774a5c005506c462c6be26532bd14e17
impbug <impbug@gmail.com>
2017-04-29 11:44:14 +0800

    hosptech

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 $(function() {
     
 });
+
+// hide menu after click menu item
+$(document).on('click','.navbar-collapse',function(e) {
+    if( $(e.target).is('a') && ( $(e.target).attr('class') != 'dropdown-toggle' ) ) {
+        $(this).collapse('hide');
+    }
+});
