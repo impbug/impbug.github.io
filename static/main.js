877ca0accefcad7856ee736c7b47754051e3d67c
impbug <impbug@gmail.com>
2018-03-12 12:33:12 +0800

    Upgraded Bootstrap to v4.

--- static/main.js
+++ static/main.js
@@ -1,7 +1,3 @@
-$(function() {
-    
-});
-
 // hide menu after click menu item
 $(document).on('click','.navbar-collapse',function(e) {
     if( $(e.target).is('a') && ( $(e.target).attr('class') != 'dropdown-toggle' ) ) {
