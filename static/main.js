28c4f61f2621a569c9a8c8ce98e9a112d014ceaf
impbug <impbug@gmail.com>
2022-03-10 17:17:18 +0800

    upgraded to bootstrap5

--- static/main.js
+++
@@ -1,6 +0,0 @@
-// hide menu after click menu item
-$(document).on('click','.navbar-collapse',function(e) {
-    if( $(e.target).is('a') && ( $(e.target).attr('class') != 'dropdown-toggle' ) ) {
-        $(this).collapse('hide');
-    }
-});
